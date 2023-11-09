898b0689e880c0b6734624e81a31bf2eca6541bd
easymikey <mikhail.avdeev@vk.team>
2023-11-09 16:01:27 +0300

    shore: delete unused code

--- packages/yew-counter/webpack.config.ts
+++ packages/yew-counter/webpack.config.ts
@@ -2,15 +2,7 @@ import path from 'path';
 import webpack from 'webpack';
 import 'webpack-dev-server';
 import WasmPackPlugin from '@wasm-tool/wasm-pack-plugin';
-// https://github.com/module-federation/module-federation-examples/blob/master/shared-store-cross-framework/react-counter/src/ReactCounter.js
 
-const federationConfig = {
-	name: 'yew_counter',
-	filename: 'remoteEntry.js',
-	exposes: {
-		'./yew': './core/pkg',
-	},
-};
 const config: webpack.Configuration = {
 	entry: './core/pkg/index.js',
 	output: {
@@ -36,7 +28,13 @@ const config: webpack.Configuration = {
 			crateDirectory: path.resolve(__dirname, 'core'),
 			outDir: path.join(__dirname, 'core/pkg'),
 		}),
-		new webpack.container.ModuleFederationPlugin(federationConfig),
+		new webpack.container.ModuleFederationPlugin({
+			name: 'yew_counter',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./yew': './core/pkg',
+			},
+		}),
 	],
 };
 
