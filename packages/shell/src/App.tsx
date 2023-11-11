3b5ec34e0ca29eaf79b878ad9122c18a9e13c5d0
easymikey <mikhail.avdeev@vk.team>
2023-11-11 18:23:31 +0300

    feat: add lazy loading for Yew component

--- packages/shell/src/App.tsx
+++ packages/shell/src/App.tsx
@@ -5,20 +5,28 @@ const ReactCounter = lazy(() => import('react_counter/ReactCounter'));
 
 const YEW_COUNTER = 'yew-counter';
 
+const YewCounter: FC<{
+	id: string;
+	onIncrement: () => void;
+	onDecrement: () => void;
+}> = ({id, onIncrement, onDecrement}) => {
+	useEffect(() => {
+		import('yew_counter/yew')
+			.then((module) => {
+				module.run_app(id, onIncrement, onDecrement);
+			})
+			.catch((err) => console.error(`Module with error: ${err}`));
+	}, []);
+
+	return <div id={id} />;
+};
+
 const App: FC = () => {
 	const [counter, setCounter] = useState(0);
 
 	const onIncrement = () => setCounter((c) => c + 1);
 
 	const onDecrement = () => setCounter((c) => c - 1);
-
-	useEffect(() => {
-		import('yew_counter/yew')
-			.then((module) => {
-				module.run_app(YEW_COUNTER, onIncrement, onDecrement);
-			})
-			.catch((err) => console.error(`Module with error: ${err}`));
-	}, []);
 
 	return (
 		<div className={styles.root}>
@@ -30,7 +38,13 @@ const App: FC = () => {
 					/>
 				</Suspense>
 				<div className={styles.counter}>{counter}</div>
-				<div id={YEW_COUNTER} />
+				<Suspense fallback="Loading YewCounter">
+					<YewCounter
+						id={YEW_COUNTER}
+						onIncrement={onIncrement}
+						onDecrement={onDecrement}
+					/>
+				</Suspense>
 			</div>
 		</div>
 	);
